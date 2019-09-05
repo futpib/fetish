e1a95de2ca571da7300778370dba0828736de758
futpib <futpib@gmail.com>
2019-09-05 15:52:12 +0300

    Upgrade dependencies

--- packages/fetish-plugin-serialize-body-to-json/src/index.js
+++ packages/fetish-plugin-serialize-body-to-json/src/index.js
@@ -2,9 +2,9 @@
 const isObject = o => o && o.toString && (o.toString() === '[object Object]');
 
 module.exports = oldFetish => options => oldFetish(
-	isObject(options.body) ?
-		Object.assign({}, options, {
+	isObject(options.body)
+		? Object.assign({}, options, {
 			body: JSON.stringify(options.body),
-		}) :
-		options
+		})
+		: options
 );
